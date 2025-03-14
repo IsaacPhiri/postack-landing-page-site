7fb0ad6b68eaec17ca73dbcfdd2684baaf253a8b
Isaac Phiri <isaacphiri315@gmail.com>
2025-03-14 17:15:14 +0200

    Remove custom webpack configuration for CSS handling in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ["style-loader", "css-loader", "postcss-loader"],
-    });
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
